e0d556b71e82dc8154b19fcf1a5d49f78d01115b
Andrew Sink <46334898+AndrewSink@users.noreply.github.com>
2025-06-27 09:04:10 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Andrew Sink',
+  description: 'Andrew Sink - Social, Projects, and More!',
+  generator: 'Andrew Sink',
 }
 
 export default function RootLayout({
